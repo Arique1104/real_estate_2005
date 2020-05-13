a3c65feadd68b6815f190e10a9ca24a7ef112036
Arique Aguilar <arique1104@gmail.com>
2020-05-13 10:28:06 -0500

    Complete Iteration 1 with passing Room Class code

--- test/room_test.rb
+++ test/room_test.rb
@@ -4,26 +4,32 @@ require './lib/room'
 
 class RoomTest < Minitest::Test
   def setup
-    @room1 = Room.new(:bedroom, 10, '13')
-    @room2 = Room.new(:living_room, 15, '12')
+    @room = Room.new(:bedroom, 10, '13')
   end
 
   def test_it_exists
-    room = Room.new(:bedroom, 10, '13')
-    assert_instance_of Room, room
+
+    assert_instance_of Room, @room
   end
 
-  def test_it_has_a_category
+  def test_it_has_readable_attributes
     # skip
     room = Room.new(:bedroom, 10, '13')
-    assert_equal :bedroom, room.category
+    assert_equal :bedroom, @room.category
+    assert_equal 10, @room.length
+    assert_equal '13', @room.width
   end
 
   def test_it_can_get_area
     # skip
-    #def initialize(category, length, width)
-
-    assert_equal 130, @room1.area
-    assert_equal 180, @room2.area
+    assert_equal 130, @room.area
   end
+
+  def test_if_room_is_painted?
+    assert_equal false, @room.is_painted?
+    @room.paint
+    assert_equal true, @room.is_painted?
+  end
+
+
 end
