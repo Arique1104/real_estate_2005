acf20cd0995ebec200892d3673ee370ea8ea4704
Arique Aguilar <arique1104@gmail.com>
2020-05-13 11:55:29 -0500

    house test w partial codes for market average, retrieve bedroom categories, and house area

--- lib/house.rb
+++ lib/house.rb
@@ -1,20 +1,44 @@
 class House
   attr_reader :price,
               :address,
-              :rooms
+              :rooms,
+              :price
 
   def initialize(price, address)
-    @price = price
+    @price = price.tr('$', '').to_i
     @address = address
     @rooms = []
-  end
-
-  def price
-
-    @price.tr('$', '').to_i
   end
 
   def add_room(room)
     @rooms << room
   end
+
+  def above_market_average?
+    if @price < 400001
+      false
+    end
+  end
+
+#   def rooms_from_category(category)
+#     @rooms.each do |room|
+#       room.category
+#     # new_category_search = []
+#     #     @rooms.each do |room|
+#     #         new_category_search << if room.category == category
+#           # end
+#
+# require 'pry'; binding.pry
+#
+#
+#   end
+    def rooms
+      @rooms.each do ||
+
+    end
+    def area
+        @house.rooms.length *
+        @house.rooms.width
+    end
+
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -8,6 +8,9 @@ class HouseTest < Minitest::Test
     @house = House.new("$400000", "123 sugar lane")
     @room_1 = Room.new(:bedroom, 10, '13')
     @room_2 = Room.new(:bedroom, 11, '15')
+    @room_3 = Room.new(:living_room, 25, '15')
+    @room_4 = Room.new(:basement, 30, '41')
+
   end
 
   def test_it_exists
@@ -22,10 +25,39 @@ class HouseTest < Minitest::Test
 
   def test_it_has_rooms
     assert_equal [], @house.rooms
+
     assert_equal [@room_1], @house.add_room(@room_1)
+
     assert_equal [@room_1, @room_2], @house.add_room(@room_2)
-# require 'pry'; binding.pry
+
     assert_equal [@room_1, @room_2], @house.rooms
+
+    assert_equal [@room_1, @room_2, @room_3], @house.add_room(@room_3)
+
+    assert_equal [@room_1, @room_2, @room_3, @room_4], @house.add_room(@room_4)
+  end
+
+  def test_it_is_above_market_average?
+    assert_equal false, @house.above_market_average?
+  end
+
+  # def test_it_can_retrieve_bedroom_categories
+  #   @house.add_room(@room_1)
+  #   @house.add_room(@room_2)
+  #   @house.add_room(@room_3)
+  #   @house.add_room(@room_4)
+  #
+  #   assert_equal [@room_1, @room_2], @house.rooms_from_category(:bedroom)
+  #
+  # end
+
+  def test_house_area
+      @house.add_room(@room_1)
+      @house.add_room(@room_2)
+      @house.add_room(@room_3)
+      @house.add_room(@room_4)
+    assert_equal 1900, @house.area
+
   end
 
 end
