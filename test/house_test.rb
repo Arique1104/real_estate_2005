a3140df5e9b9c090f4a0cc2b645c92b6b6c1757e
Arique Aguilar <arique1104@gmail.com>
2020-05-13 10:38:36 -0500

    Test it exists with passing code

--- test/house_test.rb
+++ test/house_test.rb
@@ -4,5 +4,16 @@ require './lib/room'
 require './lib/house'
 
 class HouseTest < Minitest::Test
+  def setup
+    @house = House.new("$400000", "123 sugar lane")
+  end
+
+  def test_it_exists
+    assert_instance_of House, @house
+  end
+
+  def test_it_has_readable_attributes
+    
+  end
 
 end
